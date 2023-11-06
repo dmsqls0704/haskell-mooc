4ec9c0b27c5c28e3488f1aad90b0df814059e31b
dmsqls <dmsqls0704@naver.com>
2023-11-06 10:34:33 +0900

    commit exercise 7
    Signed-off-by: dmsqls <dmsqls0704@naver.com>

--- Set2a.hs
+++ Set2a.hs
@@ -105,7 +105,8 @@ palindromify s = if s == reverse s then s
 --   safeDiv 4 0  ==> Nothing
 
 safeDiv :: Integer -> Integer -> Maybe Integer
-safeDiv x y = todo
+safeDiv x y = if y == 0 then Nothing
+              else Just (x `div` y)
 
 ------------------------------------------------------------------------------
 -- Ex 8: implement a function greet that greets a person given a first
